c78e5e85b720225ee048bd26bc6a5da540d7a9f2
Woljix <woljix@gmail.com>
2019-04-10 23:04:53 +0200

    assembly description

--- Ushanka/Properties/AssemblyInfo.cs
+++ Ushanka/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ushanka")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Top notch instagram downloader.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Woljix Software")]
 [assembly: AssemblyProduct("Ushanka")]
